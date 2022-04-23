9bb6213ba505f09a5dd3fe315ea10282f5adf388
aaaaa <gopmaster@hotmail.com>
2022-04-23 11:30:37 +0800

    角色動作傳event改用event proxy

--- Assets/PlayMaker Utils/PropertyDrawers/PlayMakerEvent/PlayMakerEvent.cs
+++ Assets/PlayMaker Utils/PropertyDrawers/PlayMakerEvent/PlayMakerEvent.cs
@@ -72,7 +72,7 @@ namespace HutongGames.PlayMaker.Ecosystem.Utils
 
 			return fsm;
 		}
-		#if UNITY_2017_1_OR_NEWER
+		#if UNITY_2017
 		public bool SendEvent(PlayMakerFSM fromFsm,PlayMakerTimelineEventTarget eventTarget,bool debug = false)
 		{
 			fromFsm = SanitizeFsmEventSender(fromFsm);
