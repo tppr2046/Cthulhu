60967e5405f66f8c04357536eb8acefd82cff1d8
Hayato <hayatos@gmail.com>
2022-12-10 15:09:36 +0800

    展場用機能：按Q播放展示影片  影片檔過大未包含在版控中，直接將影片命名demo.mp4為放置在專案或遊戲執行檔的 /StreamingAssets/ 資料夾下方即可生效

---
+++ Assets/Hayato/ApplicationGetStreamingAssetsPath.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+
+{
+    [ActionCategory(ActionCategory.Application)]
+    [Tooltip("Get the Application Streaming Assets Path")]
+    public class ApplicationGetStreamingAssetsPath : FsmStateAction
+	{
+
+		[RequiredField]
+		[Tooltip("The Application Streaming Assets Path")]
+		[UIHint(UIHint.Variable)]
+		public FsmString streamingAssetsPath;
+
+		[Tooltip("If true, adds a path delimiter '/' at the end of the path")]
+		public bool appendPathDelimiter;
+
+		public override void Reset()
+		{
+			streamingAssetsPath = null;
+			appendPathDelimiter = true;
+		}
+
+		public override void OnEnter()
+		{
+			streamingAssetsPath.Value = Application.streamingAssetsPath;
+
+			if (appendPathDelimiter)
+			{
+				streamingAssetsPath.Value += "/";
+			}
+
+			Finish();
+		}
+	}
+}
