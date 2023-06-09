5641bb57ada9da649eb24072d7c01645a586077e
Hayato <hayatos@gmail.com>
2023-06-09 17:29:54 +0800

    新增 AnimatorEvents 套件

---
+++ Assets/Ashkatchap/AnimatorEvents/Example (can be removed)/Example.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Example : MonoBehaviour {
+	public Animator animator;
+
+	void OnGUI () {
+		if (GUILayout.Button("Go to Relax")) {
+			animator.SetBool("goToRelax", true);
+		}
+
+		foreach (var elem in animator.parameters) {
+			string value = null;
+			switch (elem.type) {
+				case AnimatorControllerParameterType.Bool:
+				case AnimatorControllerParameterType.Trigger:
+					value = animator.GetBool(elem.name).ToString();
+					break;
+				case AnimatorControllerParameterType.Float:
+					value = animator.GetFloat(elem.name).ToString();
+					break;
+				case AnimatorControllerParameterType.Int:
+					value = animator.GetInteger(elem.name).ToString();
+					break;
+			}
+			GUILayout.Label("Parameter " + elem.name + ": " + value);
+		}
+	}
+
+	public void DeliberatelyThrowException() {
+		throw new System.Exception("Exception thrown from user code.");
+	}
+}
